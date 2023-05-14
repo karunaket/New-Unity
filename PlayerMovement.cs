7c652493b5bfdf77e34fb67ea2f7c2e4d5eedc7a
karunaket <karunaketan2077@gmail.com>
2023-05-14 15:09:50 +0530

    1st

---
+++ PlayerMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+    public Rigidbody rb;
+
+    public float ForwardForce = 2000f;
+    public float SidewaysForce = 2000f;
+
+
+    // We marked this as "FixedUpdate" because we are using it to mesh with physics...
+    void Update() {
+        rb.AddForce(0, 0, ForwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d")) {
+            rb.AddForce(SidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a")) {
+            rb.AddForce(-SidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
