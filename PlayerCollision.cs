d1166e6b2edc05cb608b518c035e3ab278bc0d13
karunaket <karunaketan2077@gmail.com>
2023-05-15 19:13:47 +0530

    Learned Player collision

---
+++ PlayerCollision.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+
+    // When the game object collides then the name will be printed with which it have collided...
+    /*private void OnCollisionEnter(Collision collisionInfo)
+    {
+        Debug.Log(collisionInfo.collider.name);
+    }*/
+
+
+
+    // When the game object collides then the tag will be printed with which it have collided...
+    /*private void OnCollisionEnter(Collision collisionInfo) 
+    {
+        Debug.Log(collisionInfo.collider.tag);
+    }*/
+
+
+
+    // When the game object collides with the mentioned tag i.e. Obstacle then the message will be printed
+    /*private void OnCollisionEnter(Collision collisionInfo) 
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            Debug.Log("We hit an obstacle!");
+        }
+    }*/
+
+
+
+    // When the game object collides with the mentioned tag i.e. Obstacle then the movement of the player
+    // will get disabled...
+    private void OnCollisionEnter(Collision collisionInfo) 
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+}
